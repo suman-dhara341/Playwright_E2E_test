df9fd020ee5df0de3417449efeebee1ca79c6ec9
Suman Dhara <sumandhara341@gmail.com>
2025-07-25 22:20:44 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // Optionally, make sure server settings:
+  plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0", // Especially important for CI/CD and GitHub Actions
     port: 5173,
   },
 });
+
