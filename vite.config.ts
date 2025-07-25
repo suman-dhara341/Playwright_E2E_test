13c9c8b3fa186324c80a0ebe10c8b2caeed9e5fe
Suman Dhara <sumandhara341@gmail.com>
2025-07-25 23:23:29 +0530

    yml file change

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // Especially important for CI/CD and GitHub Actions
     port: 5173,
+    strictPort: true, // Fail if port is already in use
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 4173,
+    strictPort: true,
   },
 });
 
